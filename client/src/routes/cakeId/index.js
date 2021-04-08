14e10f80b2bdac143e8a07d1efbaf87eba09ce4c
Alan Massaro <alan.massar@gmail.com>
2021-04-08 16:08:42 +0200

    responsiveness, sort and populate data

---
+++ client/src/routes/cakeId/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CakeId = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CakeId
---
+++ client/src/routes/cakes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cakes = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Cakes
